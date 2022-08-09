077c5270f5a4e3dc9f6e2a65aa7ff2071c528eaf
Edward Chisaka <edwardchisaka@Edwards-MacBook-Pro.local>
2022-08-09 03:26:55 -0700

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+};
+//updateEmployeeWithKeyAndValue(employee, key, value)
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+// destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+    
+};
+//delete non-dest'
+function deleteFromEmployeeByKey(employee, key, value) {
+    return Object.assign({}, employee, { [key]: value });
+}
+//delete dest'
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    return employee;
+};
+
+
